9589a75e3826fc74722b02ebc0b6bded59746ca6
russell <russell@ubuntu.(none)>
2012-11-05 13:18:40 -0800

    added db insert loop

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 import serial
 import MySQLdb as mdb
 import sys
+import time
+
+# database credentials
+host = 'localhost'
+username = 'root'
+password = 'password'
+database = 'serial'
+
+
 
 con = None
-
-con = mdb.connect('localhost', 'root', 'password', 'serial');
-
+con = mdb.connect(host, username, password, database);
 
 
-cur = con.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS \
-             info(id INT PRIMARY KEY AUTO_INCREMENT, \
-             temp VARCHAR(25), \
-             time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP)")
+with con:
+    cur = con.cursor()
+    cur.execute("DROP TABLE info")
+    cur.execute("CREATE TABLE IF NOT EXISTS \
+                 info(id INT PRIMARY KEY AUTO_INCREMENT, \
+                 temp DECIMAL(4,2) UNSIGNED, \
+                 humidity DECIMAL(4,2) UNSIGNED, \
+                 pressure DECIMAL(4,2) UNSIGNED, \
+                 time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP)")
+
+while True:
+    with con:
+        cur.execute("INSERT INTO info(temp) VALUES('1.0')")
+        print "inserted"
+        time.sleep(4)
